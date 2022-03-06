e8a5130f7af0a8e296c83af12dcc7bad69de34a6
Rajesh Roy <rajeshroy402@gmail.com>
2022-03-07 01:39:50 +0530

    Added dependencies

---
+++ dependencies.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+#author - rajeshroy402@gmail.com
+
+echo "We will install dependencies for DeepStream-6.0 in this process"
+sudo apt update -y
+sudo apt install libssl1.0.0 libgstreamer1.0-0 gstreamer1.0-tools gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav libgstrtspserver-1.0-0 libjansson4 gcc make git python3 -y
+sudo apt-get install gcc make git libtool autoconf autogen pkg-config cmake
+sudo apt-get install python3 python3-dev python3-pip
+sudo apt-get install linux-headers-$(uname -r)
+#Downloading NVIDIA Gefore Drivers
+cd ~/
+wget https://us.download.nvidia.com/XFree86/Linux-x86_64/470.63.01/NVIDIA-Linux-x86_64-470.63.01.run
+#Downloading CUDA 11.4
+cd ~/
+wget https://developer.download.nvidia.com/compute/cuda/11.4.3/local_installers/cuda_11.4.3_470.82.01_linux.run
+sudo apt install python3-pip -y
+sudo pip3 install gdown
+source ~/.bashrc
+cd ~/
+gdown https://drive.google.com/uc?id=1CfuV4pnEO1zx185gGCYR7G4RkTXErlNW
+cd ~/
+gdown https://drive.google.com/uc?id= #waiting to put tensorrt download link
+sudo echo -e "blacklist nouveau\noptions nouveau modeset=0" >> sudo nano /etc/modprobe.d/blacklist-nouveau.conf
+sudo update-initramfs -u
+
+echo -e "Please restart your device now: \nUse 'sudo reboot now'"
