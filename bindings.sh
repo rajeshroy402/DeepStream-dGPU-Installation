7b7bfe8a0b14e619d52d0db91dfbeefba95b4a6f
DivyankaThakur03 <85764700+DivyankaThakur03@users.noreply.github.com>
2022-03-07 01:42:27 +0530

    Update and rename install.sh to bindings.sh

--- bindings.sh
+++ bindings.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 
 cd /opt/nvidia/deepstream/deepstream/sources/
-git clone https://github.com/NVIDIA-AI-IOT/deepstream_python_apps.git
+sudo git clone https://github.com/NVIDIA-AI-IOT/deepstream_python_apps.git
 cd deepstream_python_apps/bindings/
 sudo apt install -y git python-dev python3 python3-pip python3.6-dev python3.8-dev cmake g++ build-essential \
     libglib2.0-dev libglib2.0-dev-bin python-gi-dev libtool m4 autoconf automake
-git submodule update --init
+sudo git submodule update --init
 cd ../3rdparty/gst-python/
-./autogen.sh
+sudo ./autogen.sh
 sudo make
 sudo make install
 cd ../../bindings/
 sudo mkdir build
 cd build
-cmake ..
+sudo cmake ..
 sudo make
-pip3 install ./pyds-1.1.0-py3-none*.whl
+sudo pip3 install ./pyds-1.1.0-py3-none*.whl
