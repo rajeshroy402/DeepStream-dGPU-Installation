0e98c1e1c5a0e91e6c2a0ab0a3b79b2580786056
DivyankaThakur03 <85764700+DivyankaThakur03@users.noreply.github.com>
2022-03-07 02:00:16 +0530

    Update bindings.sh

--- bindings.sh
+++ bindings.sh
@@ -2,20 +2,25 @@
 
 #author - divyanka.thakur16@gmail.com
 
-
 cd /opt/nvidia/deepstream/deepstream/sources/
+# Sync deepstream_python_apps repo from github
 sudo git clone https://github.com/NVIDIA-AI-IOT/deepstream_python_apps.git
 cd deepstream_python_apps/bindings/
+#Download base dependencies
 sudo apt install -y git python-dev python3 python3-pip python3.6-dev python3.8-dev cmake g++ build-essential \
     libglib2.0-dev libglib2.0-dev-bin python-gi-dev libtool m4 autoconf automake
+# Initialize submodules    
 sudo git submodule update --init
+# Installing Gst-python
 cd ../3rdparty/gst-python/
 sudo ./autogen.sh
 sudo make
 sudo make install
+# Now building the bindings
 cd ../../bindings/
 sudo mkdir build
 cd build
 sudo cmake ..
 sudo make
+# Installing the pip wheel
 sudo pip3 install ./pyds-1.1.0-py3-none*.whl
