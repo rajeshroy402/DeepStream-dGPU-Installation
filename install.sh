850b66d3271b19d28d11d915e0326d48237744e9
DivyankaThakur03 <85764700+DivyankaThakur03@users.noreply.github.com>
2022-03-07 01:18:36 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,7 @@ cd deepstream_python_apps/bindings/
 sudo apt install -y git python-dev python3 python3-pip python3.6-dev python3.8-dev cmake g++ build-essential \
     libglib2.0-dev libglib2.0-dev-bin python-gi-dev libtool m4 autoconf automake
 git submodule update --init
-cd ../
-cd 3rdparty/gst-python/
+cd ../3rdparty/gst-python/
 ./autogen.sh
 sudo make
 sudo make install
