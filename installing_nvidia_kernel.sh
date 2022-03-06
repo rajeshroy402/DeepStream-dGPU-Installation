df0efed995ee8db8da26b0c6d82b72055e181cfb
Rajesh Roy <rajeshroy402@gmail.com>
2022-03-07 01:51:26 +0530

    added kernel installation

---
+++ installing_nvidia_kernel.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+#author - rajeshroy402@gmail.com
+
+cd ~/nvidia-debians-by-rajesh
+# Installing NVIDIA KERNEL
+chmod 755 NVIDIA-Linux-x86_64-470.63.01.run
+sudo ./NVIDIA-Linux-x86_64-470.63.01.run
+
+echo "Please reboot your device now"
