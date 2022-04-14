9ae842bff8cb251c4b0d8c30c43537694f653a90
Rajesh Roy <rajeshroy402@gmail.com>
2022-04-14 16:36:40 +0530

    Update 04_bindings.sh

--- 04_bindings.sh
+++ 04_bindings.sh
@@ -5,8 +5,10 @@
 cd /opt/nvidia/deepstream/deepstream/sources/
 # Sync deepstream_python_apps repo from github
 sudo git clone https://github.com/NVIDIA-AI-IOT/deepstream_python_apps.git
+cd deepstream_python_apps/
+# DS 6.0
 sudo git checkout v1.1.0
-cd deepstream_python_apps/bindings/
+cd bindings/
 #Download base dependencies
 sudo apt install -y git python-dev python3 python3-pip python3.6-dev python3.8-dev cmake g++ build-essential \
     libglib2.0-dev libglib2.0-dev-bin python-gi-dev libtool m4 autoconf automake
