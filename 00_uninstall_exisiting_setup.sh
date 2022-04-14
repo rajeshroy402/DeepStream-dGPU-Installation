a01ebdaa96fad76a6eadb1c137f32de8d5243f6d
Rajesh Roy <rajeshroy402@gmail.com>
2022-04-14 16:54:41 +0530

    Update 00_uninstall_exisiting_setup.sh

--- 00_uninstall_exisiting_setup.sh
+++ 00_uninstall_exisiting_setup.sh
@@ -9,9 +9,15 @@ sudo apt -y remove --purge "^cuda.*"
 sudo apt -y remove --purge "^libnvidia.*"
 sudo apt -y remove --purge "^nvidia.*"
 sudo apt -y remove --purge "^tensorrt.*" 
+sudo apt-get remove --purge '*nvidia*'
+sudo apt-get remove --purge '*cuda*'
+sudo apt-get remove --purge '*cudnn*'
+sudo apt-get remove --purge '*tensorrt*'
+
 
 sudo apt-get -y --purge remove "*cublas*" "cuda*" "nsight*" 
 sudo apt-get -y --purge remove "*nvidia*"
 sudo apt remove -y --autoremove nvidia-cuda-toolkit
+sudo apt autoremove --purge && sudo apt autoclean && sudo apt clean
 
 echo "Kindly reboot your device"
