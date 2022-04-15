6fda67071666adee9729c82b3c7d9d28c5aed0a0
Rajesh Roy <rajeshroy402@gmail.com>
2022-04-15 14:42:19 +0530

    updated to 470103 display driver

--- 02_installing_nvidia_kernel.sh
+++ 02_installing_nvidia_kernel.sh
@@ -4,7 +4,7 @@
 
 cd ~/nvidia-debians-by-rajesh
 # Installing NVIDIA KERNEL
-chmod 755 NVIDIA-Linux-x86_64-470.63.01.run
-sudo ./NVIDIA-Linux-x86_64-470.63.01.run
+chmod 755 NVIDIA-Linux-x86_64-470.103.01.run
+sudo ./NVIDIA-Linux-x86_64-470.103.01.run
 
 echo "Please reboot your device now"
