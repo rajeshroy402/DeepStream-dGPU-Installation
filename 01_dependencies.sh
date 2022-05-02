195371ba5bb0e1e3050799fcfec699de82f7ebf4
Rajesh Roy <rajeshroy402@gmail.com>
2022-05-02 22:57:55 +0530

    added nano install command

--- 01_dependencies.sh
+++ 01_dependencies.sh
@@ -6,6 +6,7 @@ echo "We will install dependencies for DeepStream-6.0 in this process"
 rm -rf ~/nvidia-debians-by-rajesh
 mkdir ~/nvidia-debians-by-rajesh
 sudo apt update -y
+sudo apt install nano -y
 sudo apt install libssl1.0.0 libgstreamer1.0-0 gstreamer1.0-tools gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav libgstrtspserver-1.0-0 libjansson4 gcc make git python3 -y
 sudo apt-get install gcc make git libtool autoconf autogen pkg-config cmake
 sudo apt-get install python3 python3-dev python3-pip
