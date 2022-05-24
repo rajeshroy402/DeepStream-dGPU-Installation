50e966de82db7492914d27bcd207f895b5fbdae4
Rajesh Roy <rajeshroy402@gmail.com>
2022-05-25 02:33:53 +0530

    Update 02_installing_nvidia_kernel.sh

--- 02_installing_nvidia_kernel.sh
+++ 02_installing_nvidia_kernel.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
 #author - rajeshroy402@gmail.com
-
+sudo echo "Hi, let's install NVIDIA DISPLAY DRIVER here!"
 cd ~/nvidia-debians-by-rajesh
 # Installing NVIDIA KERNEL
+sudo apt install libglvnd-dev -y
+sudo apt install pkg-config -y
 chmod 755 NVIDIA-Linux-x86_64-470.103.01.run
 sudo ./NVIDIA-Linux-x86_64-470.103.01.run
 
