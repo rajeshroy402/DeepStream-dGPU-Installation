d7efbb70ca40ebe08d4aa9c36812845967a2632a
Rajesh Roy <rajeshroy402@gmail.com>
2022-05-25 01:29:25 +0530

    added libglvnd patch

--- 02_installing_nvidia_kernel.sh
+++ 02_installing_nvidia_kernel.sh
@@ -3,6 +3,8 @@
 #author - rajeshroy402@gmail.com
 sudo echo "Hi, let's install NVIDIA DISPLAY DRIVER here!"
 cd ~/nvidia-debians-by-rajesh
+sudo apt install libglvnd-dev
+sudo apt install pkg-config
 # Installing NVIDIA KERNEL
 chmod 755 NVIDIA-Linux-x86_64-510.47.03.run
 sudo ./NVIDIA-Linux-x86_64-510.47.03.run
