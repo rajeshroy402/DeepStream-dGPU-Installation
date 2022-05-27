d470db833da5d1b9d6d2195bd717d3dc6ae64195
Rajesh Roy <rajeshroy402@gmail.com>
2022-05-28 01:40:06 +0530

    Update 03_install_deepstream.sh

--- 03_install_deepstream.sh
+++ 03_install_deepstream.sh
@@ -5,7 +5,11 @@
 # Installing CUDA
 sudo echo "Hi, let's install CUDA TensorRT and DeepStream here!"
 cd ~/nvidia-debians-by-rajesh
-sudo sh cuda_11.6.1_510.47.03_linux.run
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-keyring_1.0-1_all.deb
+sudo dpkg -i cuda-keyring_1.0-1_all.deb
+sudo apt-get update
+sudo sh cuda_11.6.1_510.47.03_linux.run --silent --toolkit
 sudo printf "export PATH=/usr/local/cuda-11.6/bin${PATH:+:${PATH}}\nexport LD_LIBRARY_PATH=/usr/local/cuda-11.6/lib64\${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}" >> /home/$USER/.bashrc
 source ~/.bashrc
 sudo ldconfig
@@ -14,15 +18,13 @@ nvcc --version
 
 #Installing NVIDIA driver Optimius
 sudo apt-get install nvidia-prime
+echo "installed prime"
 sudo prime-select nvidia
 
 #Installing TensorRT
 
 cd ~/nvidia-debians-by-rajesh
 sudo rm /etc/apt/sources.list.d/*cuda*
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub
-sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
-sudo apt-get update -y
 sudo dpkg -i nv-tensorrt-repo-ubuntu2004-cuda11.4-trt8.2.5.1-ga-20220505_1-1_amd64.deb
 sudo apt-key add /var/nv-tensorrt-repo-ubuntu2004-cuda11.4-trt8.2.5.1-ga-20220505/82307095.pub
 sudo apt-get update
@@ -33,6 +35,7 @@ sudo apt install tensorrt
 cd ~/nvidia-debians-by-rajesh
 sudo apt-get install ./deepstream-6.1_6.1.0-1_amd64.deb
 rm ${HOME}/.cache/gstreamer-1.0/registry.x86_64.bin
+sudo ln -snf /usr/local/cuda-11.6 /usr/local/cuda
 
 echo "Installation is completed !!!"
 echo "Please reboot your device"
