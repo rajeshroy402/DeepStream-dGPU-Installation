1d7320407013cdea450f7935a5fe3b6f3003ba22
Rajesh Roy <rajeshroy402@gmail.com>
2022-05-28 01:36:34 +0530

    Update 00_uninstall_exisiting_setup.sh

--- 00_uninstall_exisiting_setup.sh
+++ 00_uninstall_exisiting_setup.sh
@@ -4,20 +4,23 @@
 
 sudo echo "Started uninstalling dependencies"
 
+sudo nvidia-uninstall
+sudo $CUDA_PATH/bin/cuda-uninstaller
+sudo apt-get remove --purge '*nvidia*'
+sudo apt-get remove --purge '*cuda*'
+sudo apt-get remove --purge '*cudnn*'
+sudo apt-get remove --purge '*tensorrt*'
+sudo apt autoremove --purge && sudo apt autoclean && sudo apt clean
+
 sudo apt -y remove --purge "^libcuda.*"
 sudo apt -y remove --purge "^cuda.*"
 sudo apt -y remove --purge "^libnvidia.*"
 sudo apt -y remove --purge "^nvidia.*"
 sudo apt -y remove --purge "^tensorrt.*" 
-sudo apt-get remove --purge '*nvidia*'
-sudo apt-get remove --purge '*cuda*'
-sudo apt-get remove --purge '*cudnn*'
-sudo apt-get remove --purge '*tensorrt*'
 sudo apt-get remove --purge deepstream-6.1
 
 
 sudo apt-get -y --purge remove "*cublas*" "cuda*" "nsight*" 
-sudo apt-get -y --purge remove "*nvidia*"
 sudo apt remove -y --autoremove nvidia-cuda-toolkit
 sudo apt autoremove --purge && sudo apt autoclean && sudo apt clean
 
