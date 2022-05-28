d1ac8d07cfb4f02ee011e854113c9ba2222c5642
Rajesh Roy <rajeshroy402@gmail.com>
2022-05-29 04:18:11 +0530

    Update 03_install_deepstream.sh

--- 03_install_deepstream.sh
+++ 03_install_deepstream.sh
@@ -5,11 +5,15 @@
 # Installing CUDA
 sudo echo "Hi, let's install CUDA TensorRT and DeepStream here!"
 cd ~/nvidia-debians-by-rajesh
-
+sudo apt install libglvnd-dev -y
+sudo apt install pkg-config -y
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-keyring_1.0-1_all.deb
 sudo dpkg -i cuda-keyring_1.0-1_all.deb
 sudo apt-get update -y
-sudo sh cuda_11.6.1_510.47.03_linux.run
+
+echo "let's start with CUDA-11.6"
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo printf "export PATH=/usr/local/cuda-11.6/bin${PATH:+:${PATH}}\nexport LD_LIBRARY_PATH=/usr/local/cuda-11.6/lib64\${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}" >> /home/$USER/.bashrc
 echo "Let's refresh the source file"
 source ~/.bashrc
