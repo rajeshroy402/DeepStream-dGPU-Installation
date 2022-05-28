a88d255a711bfad3f9649b2f0e94999a8a95456e
Rajesh Roy <rajeshroy402@gmail.com>
2022-05-29 05:08:30 +0530

    Update 00_uninstall_exisiting_setup.sh

--- 00_uninstall_exisiting_setup.sh
+++ 00_uninstall_exisiting_setup.sh
@@ -6,29 +6,28 @@ sudo echo "Started uninstalling dependencies"
 
 sudo nvidia-uninstall
 sudo $CUDA_PATH/bin/cuda-uninstaller
-sudo apt-get remove --purge '*nvidia*'
-sudo apt-get remove --purge '*cuda*'
-sudo apt-get remove --purge '*cudnn*'
-sudo apt-get remove --purge '*tensorrt*'
-sudo apt autoremove --purge && sudo apt autoclean && sudo apt clean
+sudo apt-get remove --purge '*nvidia*' -y
+sudo apt-get remove --purge '*cuda*' -y
+sudo apt-get remove --purge '*cudnn*' -y
+sudo apt-get remove --purge '*tensorrt*' -y
+sudo apt autoremove --purge && sudo apt autoclean && sudo apt clean -y
 
-sudo apt -y remove --purge "^libcuda.*"
-sudo apt -y remove --purge "^cuda.*"
-sudo apt -y remove --purge "^libnvidia.*"
-sudo apt -y remove --purge "^nvidia.*"
-sudo apt -y remove --purge "^tensorrt.*" 
-sudo apt-get remove --purge deepstream-6.1
+sudo apt -y remove --purge "^libcuda.*" -y
+sudo apt -y remove --purge "^cuda.*" -y
+sudo apt -y remove --purge "^libnvidia.*" -y
+sudo apt -y remove --purge "^nvidia.*" -y
+sudo apt -y remove --purge "^tensorrt.*" -y
+sudo apt-get remove --purge deepstream-6.1 -y
 
 
-sudo apt-get -y --purge remove "*cublas*" "cuda*" "nsight*" 
-sudo apt remove -y --autoremove nvidia-cuda-toolkit
-sudo apt autoremove --purge && sudo apt autoclean && sudo apt clean
+sudo apt-get -y --purge remove "*cublas*" "cuda*" "nsight*" -y 
+sudo apt remove -y --autoremove nvidia-cuda-toolkit -y
+sudo apt autoremove --purge && sudo apt autoclean && sudo apt clean -y
 
 # removing old files
 
 sudo rm -rf /usr/local/deepstream /usr/lib/x86_64-linux-gnu/gstreamer-1.0/libgstnv* /usr/bin/deepstream* /usr/lib/x86_64-linux-gnu/gstreamer-1.0/libnvdsgst*
-/usr/lib/x86_64-linux-gnu/gstreamer-1.0/deepstream*
-/opt/nvidia/deepstream/deepstream*
+/usr/lib/x86_64-linux-gnu/gstreamer-1.0/deepstream* /opt/nvidia/deepstream/deepstream*
 
 sudo rm -rf /usr/lib/x86_64-linux-gnu/libv41/plugins/libcuvidv4l2_plugin.so
 
