ee4442cac81a0c671e92e9c4a53ae93938c79e1a
Rajesh Roy <rajeshroy402@gmail.com>
2022-05-29 05:03:41 +0530

    Create 04_install-deepstream-6.1.sh

---
+++ 04_install-deepstream-6.1.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+#author - rajeshroy402@gmail.com
+
+# Installing CUDA
+sudo echo "Hi, let's install DeepStream here!"
+
+cd ~/nvidia-debians-by-rajesh
+echo "Let's install DS-6.1!"
+sudo apt-get install ./deepstream-6.1_6.1.0-1_amd64.deb
+rm ${HOME}/.cache/gstreamer-1.0/registry.x86_64.bin
+sudo ln -snf /usr/local/cuda-11.6 /usr/local/cuda
+
+echo "Installation is completed !!!"
+
+sudo prime-select nvidia
+echo "Please reboot your device"
