d238e9cd3ea18b322a54c3914f5817a53ec8b417
Rajesh Roy <rajeshroy402@gmail.com>
2022-05-30 19:07:53 +0530

    Update 02_cuda-display-driver-installation.sh

--- 02_cuda-display-driver-installation.sh
+++ 02_cuda-display-driver-installation.sh
@@ -16,9 +16,10 @@ sudo apt-get update -y
 echo "let's start with CUDA-11.6"
 #wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 #sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo rm /etc/apt/sources.list.d/*cuda*
 sudo dpkg -i cuda-repo-ubuntu2004-11-6-local_11.6.1-510.47.03-1_amd64.deb
 sudo apt-key add /var/cuda-repo-ubuntu2004-11-6-local/7fa2af80.pub
-sudo rm /etc/apt/sources.list.d/cuda-ubuntu2004-x86_64.list
+sudo rm /etc/apt/sources.list.d/*cuda*
 sudo apt-get update -y
 sudo apt-get -y install cuda
 sudo printf "export PATH=/usr/local/cuda-11.6/bin${PATH:+:${PATH}}\nexport LD_LIBRARY_PATH=/usr/local/cuda-11.6/lib64\${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}" >> /home/$USER/.bashrc
