1fb5e71987f6e56f2b908a3415ab9c6612bbdba9
Rajesh Roy <rajeshroy402@gmail.com>
2022-05-31 13:43:50 +0530

    remove prime-select and +auto-yes

--- 04_install-deepstream-6.1.sh
+++ 04_install-deepstream-6.1.sh
@@ -7,11 +7,8 @@ sudo echo "Hi, let's install DeepStream here!"
 
 cd ~/nvidia-debians-by-rajesh
 echo "Let's install DS-6.1!"
-sudo apt-get install ./deepstream-6.1_6.1.0-1_amd64.deb
+sudo apt-get install ./deepstream-6.1_6.1.0-1_amd64.deb -y
 rm ${HOME}/.cache/gstreamer-1.0/registry.x86_64.bin
 sudo ln -snf /usr/local/cuda-11.6 /usr/local/cuda
 
 echo "Installation is completed !!!"
-
-sudo prime-select nvidia
-echo "Please reboot your device"
